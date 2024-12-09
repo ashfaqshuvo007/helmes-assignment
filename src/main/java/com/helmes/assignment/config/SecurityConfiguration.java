de7bd40ea2ddaedf6cc563a5cdc91943e9cf30d1
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2024-12-09 07:26:17 +0200

    added authentication success handler.

--- src/main/java/com/helmes/assignment/config/SecurityConfiguration.java
+++ src/main/java/com/helmes/assignment/config/SecurityConfiguration.java
@@ -10,13 +10,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import static org.springframework.security.authorization.AuthorityAuthorizationManager.hasRole;
 
 @Configuration
 public class SecurityConfiguration {
 
     private final MyUserDetailService myUserDetailService;
 
-    public SecurityConfiguration(MyUserDetailService userDetailService, MyUserDetailService myUserDetailService) {
+
+    public SecurityConfiguration(MyUserDetailService myUserDetailService) {
         this.myUserDetailService = myUserDetailService;
     }
 
@@ -26,16 +30,25 @@ public class SecurityConfiguration {
         return http.authorizeHttpRequests(
             authorize-> {
                 authorize.requestMatchers("/css/**", "/js/**", "/images/**", "/fonts/**", "/login").permitAll();
-                authorize.requestMatchers("/login","/logout","/error/**","/","/home").permitAll();
+                authorize.requestMatchers("/login","/logout","/error/**").permitAll();
                 // Restrict access to admin and user pages based on roles
-                authorize.requestMatchers("/admin/**").hasRole("ADMIN");
-                authorize.requestMatchers("/user/**").hasRole("USER");
+                authorize.requestMatchers("/","/home").access(hasRole("USER"));
+                authorize.requestMatchers("/admin/**").access(hasRole("ADMIN"));
                 authorize.anyRequest().authenticated();
             }
-        ).formLogin(login -> login.loginPage("/login").defaultSuccessUrl("/", true).permitAll())
+        ).formLogin(login -> login.loginPage("/login").successHandler(customAuthenticationSuccessHandler()).permitAll())
             .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll())
             .csrf(AbstractHttpConfigurer::disable)
             .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(myUserDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
     }
 
     @Bean
@@ -49,11 +62,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(myUserDetailService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-
-        return daoAuthenticationProvider;
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationHandler();
     }
 }
